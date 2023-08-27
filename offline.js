f8b164c6e24bd11dbb1220627ed2301a0b367052
User01 <user01@gmail.com>
2023-08-27 14:43:08 +0700

    first commit

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1625502653,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/char-sheet0.png",
+		"images/char-sheet1.png",
+		"images/hair-sheet0.png",
+		"images/hair-sheet1.png",
+		"images/c-sheet0.png",
+		"images/block-sheet0.png",
+		"images/block-sheet1.png",
+		"images/particles.png",
+		"images/bg-sheet0.png",
+		"images/stair-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/imgbin_treeshrubgardendesktoppng-sheet0.png",
+		"images/tiledbackground.png",
+		"images/imgbin_ilexrotundatreelandscapegardengreenpng-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"c2mp-net.js",
+		"c2mp-peer.js",
+		"c2mp.js",
+		"waker.js"
+	]
+}
